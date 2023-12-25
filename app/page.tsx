a34986bd1976df4f2b0f90f2badf9c1ab2c3ff03
prathamdogra98 <pratham@thesilverlabs.com>
2023-12-25 19:28:27 +0530

    ADD: Build fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 export default function Home() {
   return <main className={styles.main}>This is me</main>;

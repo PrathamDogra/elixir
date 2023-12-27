e3abeada6027898bca0488cfb0c28396d9a9ffe6
prathamdogra98 <pratham@thesilverlabs.com>
2023-12-27 05:59:24 +0530

    ADD: favicon changed.

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
       <head>
         <title>{"DSR ELIXIR"}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/DSR-logo.svg" />
       </head>
       <body>
         <Suspense fallback={<Loading />}>{children}</Suspense>
